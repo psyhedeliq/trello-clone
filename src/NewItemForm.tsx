718a709f487b2182d7c99e839ab04bbcf141e3d9
Cornel-Cristian Filip <filipcornelcristian@gmail.com>
2022-10-13 23:59:01 +0300

    feat: added new item form component

---
+++ src/NewItemForm.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { NewItemButton, NewItemFormContainer, NewItemInput } from './styles';
+import { useFocus } from './utils/useFocus';
+
+type NewItemFormProps = {
+  onAdd(text: string): void;
+};
+
+export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
+  const [text, setText] = useState('');
+  const inputRef = useFocus();
+
+  return (
+    <NewItemFormContainer>
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+      <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
+    </NewItemFormContainer>
+  );
+};
