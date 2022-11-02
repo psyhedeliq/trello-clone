032c1b4ecc45e11485ca6652cb152145b90cabc1
Cornel-Cristian Filip <filipcornelcristian@gmail.com>
2022-11-03 00:48:11 +0200

    implemented useItemDrag hook

--- src/Column.tsx
+++ src/Column.tsx
@@ -2,7 +2,12 @@ import { AddNewItem } from './AddNewItem';
 import { Card } from './Card';
 import { ColumnContainer, ColumnTitle } from './styles';
 import { useAppState } from './state/AppStateContext';
-import { addTask } from './state/actions';
+import { addTask, moveList } from './state/actions';
+import { useRef } from 'react';
+import { useItemDrag } from './utils/useItemDrag';
+import { useDrop } from 'react-dnd';
+import { throttle } from 'throttle-debounce-ts';
+import { isHidden } from './utils/isHidden';
 
 type ColumnProps = {
   text: string;
@@ -10,13 +15,29 @@ type ColumnProps = {
 };
 
 export const Column = ({ text, id }: ColumnProps) => {
-  const { getTasksByListId, dispatch } = useAppState();
-
+  const { draggedItem, getTasksByListId, dispatch } = useAppState();
   const tasks = getTasksByListId(id);
-  // console.log(tasks.map((task) => task));
+  const ref = useRef<HTMLDivElement>(null);
+  const { drag } = useItemDrag({ type: 'COLUMN', id, text });
+  const [, drop] = useDrop({
+    accept: 'COLUMN',
+    hover: throttle(200, () => {
+      if (!draggedItem) {
+        return;
+      }
+      if (draggedItem.type === 'COLUMN') {
+        if (draggedItem.id === id) {
+          return;
+        }
+        dispatch(moveList(draggedItem.id, id));
+      }
+    }),
+  });
+
+  drag(drop(ref));
 
   return (
-    <ColumnContainer>
+    <ColumnContainer ref={ref} isHidden={isHidden(draggedItem, 'COLUMN', id)}>
       <ColumnTitle>{text}</ColumnTitle>
       {tasks.map((task) => {
         return <Card text={task.text} key={task.id} id={task.id} />;
